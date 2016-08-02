1cffc9c1aa2494f22bb8246337b6678c11e1e5e8
Templeton Tsai <qpzmy701117@gmail.com>
2016-08-02 11:28:43 +1000

    Init Sample Project for Docker, Nodejs

---
+++ first_nodejs_express.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var stylus = require('stylus');
+var nib = require('nib');
+// view engine setup
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+//app.use(express.logger('dev'));
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+));
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home' }
+  )
+})
+
+app.get('/sharesocialmedia', function(req, res) {
+
+	//res.render('layout', { title: 'Test' });
+	res.render('fb_share_button', { title: 'Share Button Example' });
+	//res.render('tweet_share_button', { title: 'Test' });
+	//res.send('share socail media!');
+});
+
+app.listen(3000, function() {
+	console.log('Example app listening on port 3000!');
+});
